56f24d57e17fbb02c9e001599ce2ee1146ab2457
krishna3032004 <krishnagupta30mar@gmail.com>
2025-08-10 01:23:27 +0530

    new project

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     console.log("aa rha kya")
-    const conn = await mongoose.connect(`${process.env.DATABASE_URL}/price`);
+    const conn = await mongoose.connect(`${process.env.DATABASE_URL}`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
