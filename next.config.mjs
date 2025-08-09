7b2ba1052f90f85f61bffa34db40f093087d6bc0
krishna3032004 <krishnagupta30mar@gmail.com>
2025-08-10 03:41:37 +0530

    new project

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {async rewrites() {
+//     return [
+//       { source: "/api/backend/:path*", destination: "http://localhost:8000/api/:path*" }
+//     ]
+//   },};
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {async rewrites() {
-    return [
-      { source: "/api/backend/:path*", destination: "http://localhost:8000/api/:path*" }
-    ]
-  },};
+const nextConfig = {
+  webpack: (config) => {
+    // .map files ko ignore kare
+    config.module.rules.push({
+      test: /\.map$/,
+      use: 'ignore-loader'
+    });
+    return config;
+  }
+};
 
 export default nextConfig;
-
-
